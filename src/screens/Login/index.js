00aa49fe20a7259728a190c3e77a9518df28b6f6
Lucas Medeiros <lucasmmedeirosm@gmail.com>
2023-04-07 01:26:38 -0300

    Organização pastas

---
+++ src/screens/Login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class Login extends React.Component {
+    render() {
+        return(
+            <View style={styles.container}>
+                
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    },
+  });
